973d713c18fc3661ecccbb9d06af87e7a436f0f5
Nitish S <nitiships14@gmail.com>
2026-06-29 20:52:04 +0530

    Update next.config.ts for GitHub Pages support

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
+const repo = "VISUAL_CODE_PORTFOLIO_WEBSITE-"; // Your GitHub repository name
 
 const nextConfig: NextConfig = {
   ...(isGithubPages && {
-    output: 'export',
-    basePath: '/vscp',
-    assetPrefix: '/vscp/',
+    output: "export",
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}/`,
     trailingSlash: true,
-    images: { unoptimized: true },
+    images: {
+      unoptimized: true,
+    },
   }),
 };
 
